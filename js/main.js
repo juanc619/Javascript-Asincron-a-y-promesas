f1079aaf4635098cbf0ce2ce10eda7066dca78a0
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-07-17 19:55:50 -0500

    Promise

--- js/main.js
+++ js/main.js
@@ -7,14 +7,28 @@
 
 // console.log("Chau");
 
-let contador = 0;
+// let contador = 0;
 
-const intervalo = setInterval(() => {
-    contador++;
-    console.log(contador);
+// const intervalo = setInterval(() => {
+//     contador++;
+//     console.log(contador);
 
-    if (contador === 5) {
-        clearInterval(intervalo);
-        console.log("El contador se freno");
-    }
-}, 1000);
+//     if (contador === 5) {
+//         clearInterval(intervalo);
+//         console.log("El contador se freno");
+//     }
+// }, 1000);
+
+const eventoFuturo = (res) => {
+    return new Promise ((resolve, reject) => {
+        if (res === true) {
+            resolve("Promesa resuelta");
+        } else {
+            reject("Promesa rechazada");
+        }
+    })
+}
+
+const valor = true;
+
+console.log(eventoFuturo(valor) );
