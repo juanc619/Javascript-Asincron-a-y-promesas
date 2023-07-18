175fd2b68cde8a7cc79333acd5c8229bb3346b34
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-07-17 19:21:30 -0500

    setInterval

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 
-console.log("Hola!!");
+// console.log("Hola!!");
 
-setTimeout(() => {
-    console.log("plop!")
-}, 4000);
+// setTimeout(() => {
+//     console.log("plop!")
+// }, 4000);
 
-console.log("Chau");
+// console.log("Chau");
+
+let contador = 0;
+
+const intervalo = setInterval(() => {
+    contador++;
+    console.log(contador);
+
+    if (contador === 5) {
+        clearInterval(intervalo);
+        console.log("El contador se freno");
+    }
+}, 1000);
